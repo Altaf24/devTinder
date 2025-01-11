c41cd63d58bf731c52b7b29306c89e92a1b7cde8
Altaf24 <tambolialtaf1419@gmail.com>
2025-01-11 15:04:07 +0530

    update user schema with validations

--- src/app.js
+++ src/app.js
@@ -6,28 +6,28 @@ const User = require('./models/user');
 app.use(express.json());
 
 
-// app.post('/signup',async(req,res)=>{
-//     // console.log(req.body);
-//     // const userObj = {
-//     //     firstName: "Virat",
-//     //     lastName: "Kohli",
-//     //     email: "virat@gmail.com",
-//     //     password: "virat@123",
-//     //     gender: "male",
-//     //     age: 35,
-//     // }
-//     // Creating a new instnce of User model
-//     const user = new User(req.body);
+app.post('/signup',async(req,res)=>{
+    // console.log(req.body);
+    // const userObj = {
+    //     firstName: "Virat",
+    //     lastName: "Kohli",
+    //     email: "virat@gmail.com",
+    //     password: "virat@123",
+    //     gender: "male",
+    //     age: 35,
+    // }
+    // Creating a new instnce of User model
+    const user = new User(req.body);
 
-//     try{
-//         await user.save();
-//         res.send('User created successfully');
-//     }catch(err){
-//         res.status(500).send("Error creating user");
-//     }
+    try{
+        await user.save();
+        res.send('User created successfully');
+    }catch(err){
+        res.status(500).send("Error creating user");
+    }
 
     
-// })
+})
 
 // Feed API - Get all users from DB
 
@@ -39,6 +39,30 @@ app.get('/feed',async(req,res)=>{
         res.status(500).send("Error fetching users");
     }
 });
+
+app.delete("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.status(500).send("Error deleting user");
+    }
+
+
+})
+
+// update user
+app.patch('/user',async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after",runValidators:true,});
+        res.send("User updated successfully");
+    }catch(err){
+        res.status(500).send("Error updating user");
+    }
+})
 
 connectDB().then(()=>{
     console.log('DB connected');
--- src/models/user.js
+++ src/models/user.js
@@ -3,29 +3,60 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 20,
        
     },
     lastName: {
         type: String,
+       
         
     },
     email: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
         
     },
     password: {
         type: String,
+        required: true,
+
        
     },
     gender: {
         type: String,
+        validate(value) {
+            if(!['male', 'female', 'others'].includes(value)) {
+                throw new Error('Invalid gender');
+            }
+        }
         
     },
     age: {
         type: Number,
+        min: 18,
+        max: 60,
         
     },
+    photoUrl: {
+        type: String,
+
+    },
+    about: {
+        type: String,
+        default: "Hey there! I am using DevTinder",
+
+    },
+    skills: {
+        type: [String],
+    },
     
+},{
+    timestamps: true,
 });
 
 const userModel = mongoose.model('User', userSchema);
